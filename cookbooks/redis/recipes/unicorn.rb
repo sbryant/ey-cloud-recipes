42dd453ab6db0522bf0ff1fb4f3cbe25dbb43c77
Sean Bryant <sean@vitrue.com>
2011-04-12 15:31:49 -0400

    add do the env.custom correctly

--- cookbooks/redis/recipes/unicorn.rb
+++ cookbooks/redis/recipes/unicorn.rb
@@ -3,6 +3,15 @@ if ['util', 'app', 'app_master'].include?(node[:instance_role])
     owner "deploy"
     group "deploy"
     mode 0644
+    source "env.custom"
+    backup false
+    action :create
+  end
+
+  remote_file "/data/SeattlesBest/shared/config/custom_unicorn.rb" do
+    owner "deploy"
+    group "deploy"
+    mode 0644
     source "unicorn.rb"
     backup false
     action :create
